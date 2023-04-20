b7a63ad08800fe04770cebe9fbc8665e506e48a3
jelarson <jesslarson93@gmail.com>
2023-04-19 21:36:36 -0600

    worker threads

--- threads.js
+++ threads.js
@@ -1,1 +1,17 @@
-require('worker_threads');
+const {
+  isMainThread, 
+  workerData,
+  Worker} = require('worker_threads');
+
+if (isMainThread) {
+  console.log('Main Thread! Process ID: ' + process.pid);
+  new Worker(__filename, {
+    workerData: [7,6,2,3]
+  });
+  new Worker(__filename, {
+    workerData: [1,3,4,3]
+  });
+} else {
+  console.log('Worker! Process ID: ' + process.pid);
+  console.log(workerData + 'sorted is ' + workerData.sort())
+}
